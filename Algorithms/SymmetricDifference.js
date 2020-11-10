0f90360359fcad1dcd82e44eed78d5bd961300a3
unknown <valdrinnimani@outlook.de>
2020-11-10 09:03:37 +0100

    Finished Symmetric Difference

--- Algorithms/SymmetricDifference.js
+++ Algorithms/SymmetricDifference.js
@@ -1,26 +1,27 @@
 function sym(...args) {
     if(args.length == 1) {
-        return args
+        return args[0].sort()
    }    
    let returnSet = [];
-   args[0] = args[0].filter((v,i,a)=>a.indexOf(v)==i)
-   args[1] = args[1].filter((v,i,a)=>a.indexOf(v)==i)
+   args1 = args[0].filter((v,i,a)=>a.indexOf(v)==i)
+   args2 = args[1].filter((v,i,a)=>a.indexOf(v)==i)
 
-    for (let i = 0; i < args[0].length; i++) {
-        console.log(args[0])
-        console.log(args[1])
-        for (let j = 0; j < args[1].length; j++) {
-            if (args[0][i] == args[1][j]) {
-               args[0].splice(i, 1)
-               args[1].splice(j, 1)
+
+   args1Copy = [...args1];
+   args2Copy = [...args2];
+   
+    for (let i = 0; i < args1.length; i++) {
+        for (let j = 0; j < args2.length; j++) {
+            if (args1[i] == args2[j]) {
+               args1Copy.splice(args1Copy.indexOf(args1[i]), 1)
+               args2Copy.splice(args2Copy.indexOf(args1[i]), 1)
             }
         }
     }
-    // console.log(args[0])
-    // console.log(args[1])
-    returnSet = args[0].concat(args[1]) 
+    
+    returnSet = args1Copy.concat(args2Copy)
     result = [returnSet, ...args.slice(2)]
     return sym(...result) 
 }
 
-  console.log(sym([1, 2, 3, 3], [5, 2, 1, 4]))
+  console.log(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]))
