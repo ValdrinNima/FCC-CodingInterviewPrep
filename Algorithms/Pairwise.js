cced02fd80229e0a05c3e086591b31c777d73d65
unknown <valdrinnimani@outlook.de>
2020-11-13 09:02:13 +0100

    Finished Pairwise

--- Algorithms/Pairwise.js
+++ Algorithms/Pairwise.js
@@ -1,11 +1,21 @@
 function pairwise(arr, arg) {
-  
-    for (let i = 0; i < arr.length; i++) {
-        
-        for (let j = 0; j < arr.length; j++) {
-            
-        }
-    }
+	let sum = 0;
+	let alreadyUsed = [];
+
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = i + 1; j < arr.length; j++) {
+			if (alreadyUsed.includes(j) || alreadyUsed.includes(i)) {
+				continue;
+			}
+			if (arr[i] + arr[j] == arg) {
+				sum += i + j;
+				alreadyUsed.push(j);
+				break;
+			}
+		}
+	}
+
+	return sum;
 }
 
-console.log(pairwise([1,4,2,3,0,5], 7));
+console.log(pairwise([1, 4, 2, 3, 0, 5], 7));
