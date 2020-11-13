fbcd65eb671d4f8de06b800b2017854e8ceeb712
unknown <valdrinnimani@outlook.de>
2020-11-13 09:39:10 +0100

    Finished SelectionSort

--- Algorithms/BubbleSort.js
+++ Algorithms/BubbleSort.js
@@ -1,9 +1,7 @@
 function bubbleSort(arr) {
 	for (let i = 0; i < arr.length; i++) {
 		for (let j = i + 1; j < arr.length; j++) {
-			if (arr[i] <= arr[j]) {
-				continue;
-			} else {
+			if (arr[i] > arr[j]) {
 				[arr[i], arr[j]] = [arr[j], arr[i]];
 			}
 		}
---
+++ Algorithms/SelectionSort.js
@@ -0,0 +1,39 @@
+function selectionSort(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		let smallest = arr[i];
+		let smallestIndex = i;
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[j] < smallest) {
+				smallest = arr[j];
+				smallestIndex = j;
+			}
+			if (j == arr.length - 1) {
+				[arr[i], arr[smallestIndex]] = [smallest, arr[i]];
+			}
+		}
+	}
+
+	return arr;
+}
+
+console.log(
+	selectionSort([
+		1,
+		4,
+		2,
+		8,
+		345,
+		123,
+		43,
+		32,
+		5643,
+		63,
+		123,
+		43,
+		2,
+		55,
+		1,
+		234,
+		92,
+	])
+);
