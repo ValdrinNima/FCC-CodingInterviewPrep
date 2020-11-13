0d4b451c63b7365fa7c2ed0b4175102f04c97e5d
unknown <valdrinnimani@outlook.de>
2020-11-13 09:11:28 +0100

    Finished BubbleSort

---
+++ Algorithms/BubbleSort.js
@@ -0,0 +1,34 @@
+function bubbleSort(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[i] <= arr[j]) {
+				continue;
+			} else {
+				[arr[i], arr[j]] = [arr[j], arr[i]];
+			}
+		}
+	}
+	return arr;
+}
+
+console.log(
+	bubbleSort([
+		1,
+		4,
+		2,
+		8,
+		345,
+		123,
+		43,
+		32,
+		5643,
+		63,
+		123,
+		43,
+		2,
+		55,
+		1,
+		234,
+		92,
+	])
+);
