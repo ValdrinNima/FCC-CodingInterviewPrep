95a32ff7154b2ee5f48f48e44235151c2d7f5591
unknown <valdrinnimani@outlook.de>
2020-11-23 19:03:30 +0100

    started Mergesort

--- Algorithms/MergeSort.js
+++ Algorithms/MergeSort.js
@@ -1,7 +1,21 @@
-function mergeSort(arr) {
+function mergeSort(arr) {}
 
+function merge(arr1, arr2) {
+	let result = [];
+	let i = 0;
+	while (arr1.length > 0 && arr2.length > 0) {
+		if (arr1[i] < arr2[i]) {
+			result.push(arr1[i]);
+			arr1.splice(i, 1);
+		} else {
+			result.push(arr2[i]);
+			arr2.splice(i, 1);
+		}
+	}
+	let longer = arr2.length > arr1.length ? arr2 : arr1;
+	result.push(...longer);
 
-    return arr
+	return result;
 }
 
 mergeSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]);
