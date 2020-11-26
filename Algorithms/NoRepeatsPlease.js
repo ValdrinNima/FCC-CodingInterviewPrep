9504d82b65ae1061306e1827d2d2821605235b7f
unknown <valdrinnimani@outlook.de>
2020-11-26 17:19:45 +0100

    Finished noreaptsplease

--- Algorithms/NoRepeatsPlease.js
+++ Algorithms/NoRepeatsPlease.js
@@ -1,44 +1,36 @@
-// function permAlone(str) {
-//   let arr = str.split("");
+function permAlone(str) {
+	let input = str.split("");
+	var permArr = [],
+		usedChars = [];
 
-//   function perm(arr, permutation = []) {
+	function permute(input) {
+		var i, ch;
+		for (i = 0; i < input.length; i++) {
+			ch = input.splice(i, 1)[0];
+			usedChars.push(ch);
+			if (input.length == 0) {
+				permArr.push(usedChars.slice());
+			}
+			permute(input);
+			input.splice(i, 0, ch);
+			usedChars.pop();
+		}
+		return permArr;
+	}
 
-//     if (arr.length == 1) {
-//         result.push(arr[0])
-//     }
-
-//     for (let i = 0; i < arr.length; i++) {
-//       permutation.push(arr[i]);
-
-//       perm(arr.slice(1,), permutation);
-//     }
-//   }
-// }
-let result = [];
-
-function perm(arr, permutation=[]) {
-    let result = []
-    
-    if(arr.length == 0) {
-        console.log(permutation)
-       return permutation 
-    }
-    
-    for (let i = 0; i < arr.length; i++) {
-        if (arr.length != 0) {
-            let newArr = [...arr]
-            permutation.push(arr[i])
-            newArr.splice(i, 1)
-            result.push(perm(newArr, permutation))
-        } else {
-            let newArr = [...arr]
-            permutation.push(arr[i])
-            newArr.splice(i, 1)
-            return perm(newArr, permutation)
-        }
-    }
-    return result
+	let permutations = permute(input);
+	console.log(permutations);
+	let result = permutations;
+	for (let i = 0; i < permutations.length; i++) {
+		for (let j = 0; j < permutations[i].length - 1; j++) {
+			if (permutations[i][j] === permutations[i][j + 1]) {
+				permutations.splice(i, 1);
+				i -= 1;
+				break;
+			}
+		}
+	}
+	return permutations.length;
 }
 
-
-console.log(perm([1, 2, 3]));
+console.log(permAlone("aabb"));
